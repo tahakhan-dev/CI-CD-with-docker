226395fd4037c62e05d3579bdd6f0d3d35ad72c3
tahakhan-dev <mtahakhan.dev@gmail.com>
2021-09-05 19:56:54 +0500

    ..gf

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome</h1>
             <Link to="/">Home</Link>
-            <Link to="/otherpage">Other </Link>
+            <Link to="/otherpage">Othe</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
